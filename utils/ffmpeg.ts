e563c2e95041f11295aace351a23faf29233893c
Alevante <alejandrovelasquezmolina@gmail.com>
2026-05-12 15:54:09 -0500

    fix: problema action y react native reanaimated

--- utils/ffmpeg.ts
+++ utils/ffmpeg.ts
@@ -1,4 +1,4 @@
-import { FFmpegKit, FFprobeKit, ReturnCode } from 'ffmpeg-kit-react-native';
+import { FFmpegKit, FFprobeKit, ReturnCode } from '@wekor/react-native-ffmpeg';
 import { Paths } from 'expo-file-system';
 import * as FileSystem from 'expo-file-system';
 
